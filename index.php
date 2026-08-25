499f0ebd7036cecb81f562c535d8e4f3b73f18ca
Kokimoto <suportekokimoto@gmail.com>
2026-08-25 11:43:02 -0300

    fix: session_start() robusto para Vercel serverless + tabelas faltantes criadas

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 ini_set('display_errors', 0);
 error_reporting(E_ALL);
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    try { @session_start(); } catch (\Throwable $e) {}
+}
 header('Content-Type: text/html; charset=utf-8');
 require_once "config.php";
 require_once DASH . "/services/database.php";
